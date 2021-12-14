ad76d29d3f260a6636796e2f7d49f5051235ddec
krisz501@sulid.hu <krisz501@sulid.hu>
2021-12-14 11:32:50 +0100

    ProgramVersion page létrejötte

--- Desktop/src/Vizsgaremek/Views/MainWindow.xaml.cs
+++ Desktop/src/Vizsgaremek/Views/MainWindow.xaml.cs
@@ -55,6 +55,8 @@ namespace Vizsgaremek
                     case "lviExit":
                         Close();
                         break;
+                   
+
                 }
             }
         }
